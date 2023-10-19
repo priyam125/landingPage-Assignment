0dd5a323b3a6f6fd03356774acad7efae0b2ac9a
priyam125 <sinha.priyam002@gmail.com>
2023-10-20 04:48:31 +0530

    View landing page with components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-200">
+      <div className="container mx-auto p-4">
+        <p className="text-center text-gray-600">
+          &copy; 2023 Your Brand. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,66 @@
+// import React, { useEffect, useState } from "react";
+// import { useParams, Link } from "react-router-dom";
+
+// function ViewLandingPage() {
+//   const { id } = useParams();
+//   const [landingPage, setLandingPage] = useState(null);
+
+//   useEffect(() => {
+//     // Load landing page data for the specified id from storage
+//     const loadedLandingPage = localStorage.getItem("landingPages") || "[]";
+//     const landingPages = JSON.parse(loadedLandingPage);
+//     const selectedLandingPage = landingPages.find(
+//       (page) => page.id === parseInt(id, 10)
+//     );
+
+//     if (selectedLandingPage) {
+//       setLandingPage(selectedLandingPage);
+//     }
+//   }, [id]);
+
+//   if (!landingPage) {
+//     // Handle the case where the landing page with the given ID is not found
+//     return <div>Landing Page not found.</div>;
+//   }
+
+//   return (
+//     <div className="container mx-auto px-4 mt-4">
+//       <h1 className="text-2xl font-bold mb-4">View Landing Page</h1>
+//       <div>
+//         <label className="block text-gray-600 font-bold">Title:</label>
+//         <div>{landingPage.title}</div>
+//       </div>
+//       <div>
+//         <label className="block text-gray-600 font-bold">Description:</label>
+//         <div>{landingPage.description}</div>
+//       </div>
+//       <div>
+//         <label className="block text-gray-600 font-bold">Components:</label>
+//         <div>
+//           {landingPage && landingPage.components ? (
+//             landingPage.components.map((component) => (
+//               <div key={component}>{component}</div>
+//             ))
+//           ) : (
+//             <div>No components found.</div>
+//           )}
+//         </div>
+//       </div>
+//       <Link to="/dashboard">
+//         <button className="bg-blue-500 text-white p-2 rounded mt-4">
+//           Back to Dashboard
+//         </button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+// export default ViewLandingPage;
+
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function ViewLandingPage() {
   const { id } = useParams();
@@ -23,29 +84,15 @@ function ViewLandingPage() {
     return <div>Landing Page not found.</div>;
   }
 
+  const hasHeader = landingPage.components && landingPage.components.includes("Header");
+  const hasFooter = landingPage.components && landingPage.components.includes("Footer");
+
   return (
     <div className="container mx-auto px-4 mt-4">
-      <h1 className="text-2xl font-bold mb-4">View Landing Page</h1>
-      <div>
-        <label className="block text-gray-600 font-bold">Title:</label>
-        <div>{landingPage.title}</div>
-      </div>
-      <div>
-        <label className="block text-gray-600 font-bold">Description:</label>
-        <div>{landingPage.description}</div>
-      </div>
-      <div>
-        <label className="block text-gray-600 font-bold">Components:</label>
-        <div>
-          {landingPage && landingPage.components ? (
-            landingPage.components.map((component) => (
-              <div key={component}>{component}</div>
-            ))
-          ) : (
-            <div>No components found.</div>
-          )}
-        </div>
-      </div>
+      {hasHeader && <Header />}
+      <h1 className="text-2xl font-bold mb-4">{landingPage.title}</h1>
+      <div>{landingPage.description}</div>
+      {hasFooter && <Footer />}
       <Link to="/dashboard">
         <button className="bg-blue-500 text-white p-2 rounded mt-4">
           Back to Dashboard
@@ -56,3 +103,4 @@ function ViewLandingPage() {
 }
 
 export default ViewLandingPage;
+
