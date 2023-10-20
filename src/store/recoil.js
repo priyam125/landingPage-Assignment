327eb4285522303788a889ae1d921ff6d0d57c14
priyam125 <sinha.priyam002@gmail.com>
2023-10-20 23:29:50 +0530

    fix

--- src/store/recoil.js
+++ src/store/recoil.js
@@ -1,15 +1,1 @@
 import { atom } from "recoil";
-
-export const isAuthenticatedState = atom({
-  key: "isAuthenticatedState",
-  default: false, // Set the initial state to false (not authenticated)
-});
-
-export const landingPagesState = atom({
-  key: "landingPages",
-  default: [
-    { id: 1, title: "Landing Page 1", description: "Description 1" },
-    { id: 2, title: "Landing Page 2", description: "Description 2" },
-    // Add more landing pages as needed
-  ],
-});
